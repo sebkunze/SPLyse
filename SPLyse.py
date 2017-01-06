b891fbe29b782d30e3b95f4c0cb15d5e8dd80963
Sebastian Kunze <sebastian.kunze@hh.se>
2017-01-06 13:14:46 +0100

    Introduce logging functionality.

--- SPLyse.py
+++ SPLyse.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import os, sys
 
-from subprocess import call
-from core.logger import info
+from subprocess     import call
+from core.logger    import info, debug
 
-workspace = os.path.join(os.getcwd(),'.workspace')
+workspace  = os.path.join(os.getcwd(),'.workspace')
+symbooglix = 'exec.bpl'
 
 def main():
     if len(sys.argv) < 2:
@@ -14,54 +15,63 @@ def main():
     # retrieve base path of software product line!
     path = sys.argv[1]
 
-    if not os.path.exists(workspace): # TODO: Clear workspace!
+    if not os.path.exists(workspace):
         os.makedirs(workspace, 0755)
 
     # browse all source folders of the software product line variants.
-    source_folders = os.listdir(path)
-    for folder in source_folders:
+    source_folder = os.listdir(path)
+    for folder in source_folder:
+        # ignore MacOS' custom attributes of the analysed folder.
+        if folder == '.DS_Store':
+            continue
+
         info('Processing all source files within folder %s', folder)
 
         # browse all source files of each software product line variant.
         for root, dirs, files in os.walk(os.path.join(path, folder)):
-            for file in files:
-                if '.java' in file:
-                    info('Translating source file %s', file)
+            # filter Java files.
+            source_files = [f for f in files if '.java' in f]
 
-                    source = os.path.join(root, file)
-                    # TODO: Find more readable implementation!
-                    target = os.path.join(workspace, folder, file).replace('.java','.bpl')
+            for f in source_files:
+                info('Translating source file %s', f)
 
-                    if not os.path.exists(os.path.join(workspace, folder)):
-                        os.mkdir(os.path.join(workspace, folder), 0755)
+                # specify source and target files for Java2Boogie.
+                source = os.path.join(root, f)
+                target = os.path.join(workspace, folder, f).replace('.java','.bpl')
 
-                    # Call Java2Boogie parser with respective options.
-                    call(['Java2Boogie', '-s', source, '-t', target])
+                # create folder if non-existing.
+                if not os.path.exists(os.path.join(workspace, folder)):
+                    os.mkdir(os.path.join(workspace, folder), 0755)
+
+                # call Java2Boogie parser with respective options.
+                call(['Java2Boogie', '-s', source, '-t', target])
 
     # browse all translated files of each software product line variant.
     parsed_source_folders = os.listdir(workspace)
     for folder in parsed_source_folders:
-        info('Merging all translated source files in %s', folder)
+        # ignore MacOS' custom attributes of the analysed folder.
+        if folder == '.DS_Store':
+            continue
+
+        info('Merging all translated source files in folder %s', folder)
 
         for root, dirs, files in os.walk(os.path.join(workspace, folder)):
-
-            # Build the name for boogie source file, i.e., the software product line variant's name.
-            name = os.path.split(root)[-1] + '.bpl'
-
-            for file in [f for f in files if not f == name]:
-
+            for f in [f for f in files if not f == symbooglix]:
                 # Append boogie source files.
-                source = open(os.path.join(root, file), 'r')
-                target = open(os.path.join(root, name), 'a')
+                source = open(os.path.join(root, f),          'r')
+                target = open(os.path.join(root, symbooglix), 'a')
 
                 target.write(source.read())
 
-    # TODO: Set glaobal variable for symbooglix!
-    # run symbooglix on given files
-    # for folder in parsed_source_folders:
-    #     for root, dirs, files in os.walk(os.path.join(workspace, folder)):
-    #         if file == symbooglix:
-    #             call([symbooglix, os.path.join(root, file)])
+    # run symbooglix on merged files.
+    for folder in parsed_source_folders:
+        for root, dirs, files in os.walk(os.path.join(workspace, folder)):
+            for f in files:
+                if f == symbooglix:
+                    info('Analysing variant in folder %s', folder)
+
+                    source = os.path.join(root, f)
+                    call(['mono $SYMBOOGLIX_HOME/Debug/sbx.exe ' + source + ' --esi-show-constraints 1 --esi-show-vars 1 --write-smt2 0 --max-depth 1000 --output-dir out'], shell=True)
 
 if __name__ == '__main__':
     main()
--- core/logger.py
+++ core/logger.py
@@ -1,12 +1,17 @@
 import logging
 
+filename = 'SPLyse.log'
+
 def info(msg, args = []):
     log.info(msg, args)
+
+def debug(msg, args = []):
+    log.debug(msg, args)
 
 log = logging.getLogger('SPLyse')
 log.setLevel(logging.DEBUG)
 
-fLog = logging.FileHandler('SPLyse.log')
+fLog = logging.FileHandler(filename)
 fLog.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
